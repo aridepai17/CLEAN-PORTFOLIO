aa23d3ba690d1cc416d2745f614c7737444c66b2
aridepai17 <advaithdepai26@gmail.com>
2026-07-25 23:07:14 +0530

    refactor: increase blog grid to 3 columns on large screens
    Changes lg:grid-cols-2 to lg:grid-cols-3 in BlogList layout for improved use of wide viewports.

--- src/components/blog/BlogList.tsx
+++ src/components/blog/BlogList.tsx
@@ -21,7 +21,7 @@ export function BlogList({ posts, className = '' }: BlogListProps) {
 
     return (
         <div
-            className={`grid gap-6 md:grid-cols-2 lg:grid-cols-2 ${className}`}
+            className={`grid gap-6 md:grid-cols-2 lg:grid-cols-3 ${className}`}
         >
             {posts.map((post) => (
                 <BlogCard key={post.slug} post={post} />
